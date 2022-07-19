0f7bb596463dfcc6c257c2050aa53710d0a09764
William Makau <williammakau070@gmail.com>
2022-07-19 15:09:12 +0300

    added pagination on transactions

--- src/main/java/com/william/lendtech/transaction/TransactionService.java
+++ src/main/java/com/william/lendtech/transaction/TransactionService.java
@@ -1,29 +1,7 @@
-package com.william.lendtech.transaction;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.List;
-
-/**
+package com.william.lendtech.transaction;/**
  * @author william makau
  * @version 1.0.0
  * Date 2022-07-19
- */
-@Service
-@RequiredArgsConstructor
-@Transactional
-@Slf4j
-public class TransactionService {
-    private final TransactionRepository transactionRepository;
-
-    public Transaction saveTransaction(Transaction transaction) {
-        return transactionRepository.save(transaction);
-    }
-
-    public List<Transaction> getAll() {
-        return (List<Transaction>) transactionRepository.findAll();
-    }
+ * Email: william.k.makau@gmail.com
+ */public interface TransactionService {
 }
